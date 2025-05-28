0c1c8d01e109aa0f2e69bd4251ffb4c6aa8e623e
RDX463 <balsarafrohan627@gmail.com>
2025-05-28 12:08:06 +0530

    Revert "redwood: Switch to NCM USB tethering"
    This reverts commit c91c3a73d8f40a52225faacb6aa82e8b2450c72d.

--- device.mk
+++ device.mk
@@ -307,9 +307,6 @@ PRODUCT_PACKAGES += \
     redwoodINWifiOverlay \
     redwoodNfcOverlay
 
-PRODUCT_PACKAGES += \
-    NcmTetheringOverlay
-
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
