3f3806598795ad1e152784de8b75b4b9d12c4490
Mayur U <107454947+Mayur-U@users.noreply.github.com>
2025-06-14 21:43:58 +0530

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/Redwood-trees/proprietary_vendor_xiaomi_redwood vendor/xiaomi/redwood
+
+git clone https://github.com/Redwood-trees/vendor_xiaomi_redwood-miuicamera.git vendor/xiaomi/redwood-miuicamera
+
+git clone https://github.com/Redwood-trees/android_hardware_xiaomi hardware/xiaomi
+
+git clone https://github.com/Atom-X-Devs/scarlet_xiaomi_sm7325.git kernel/xiaomi/redwood
+
+git clone https://github.com/Redwood-trees/vendor_xiaomi_redwood-firmware.git vendor/xiaomi/redwood-firmware
+
+git clone https://github.com/Redwood-trees/hardware_dolby.git hardware/dolby
+
+git clone https://github.com/Mayur-U/vendor-lineage-priv.git vendor/lineage-priv
